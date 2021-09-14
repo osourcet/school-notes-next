2ac64f9005d9fc8b48a5638977878715e5f27117
felix-07-11 <felixwochnick@gmail.com>
2021-09-15 00:31:14 +0200

    fix server

--- backend/src/notes/notes.module.ts
+++ backend/src/notes/notes.module.ts
@@ -5,10 +5,17 @@ import { NotesService } from './notes.service';
 import { GqlClientService } from 'src/gql-client/gql-client.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { AuthService } from 'src/auth/auth.service';
+import { MarkdownToPdfService } from '../markdown-to-pdf/markdown-to-pdf.service';
 
 @Module({
     imports: [AuthModule],
-    providers: [NotesGateway, NotesService, GqlClientService, AuthService],
+    providers: [
+        NotesGateway,
+        NotesService,
+        GqlClientService,
+        AuthService,
+        MarkdownToPdfService,
+    ],
     controllers: [NotesController],
     exports: [NotesGateway],
 })
