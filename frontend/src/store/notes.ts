9f0674ed1c2ca17716b74e138672a916658d840f
felix-07-11 <felixwochnick@gmail.com>
2021-09-17 00:44:02 +0200

    update dockerfile & add SocketIO to frontend

--- frontend/src/store/notes.ts
+++ frontend/src/store/notes.ts
@@ -1,8 +1,10 @@
 import { AxiosInstance } from 'axios';
 import { Module } from 'vuex';
+import { io, Socket } from 'socket.io-client';
 
 interface State {
     notes: Note[];
+    socket: Socket | null;
 }
 
 const noteModule = {
@@ -10,10 +12,11 @@ const noteModule = {
 
     state: {
         notes: [],
+        socket: null,
     },
 
     actions: {
-        async init({ state }) {
+        async init({ state, dispatch }) {
             try {
                 const { data } = await (
                     this.getters.axios as AxiosInstance
@@ -24,7 +27,81 @@ const noteModule = {
                 });
 
                 state.notes = [...data.own, ...data.readonly];
-            } catch (error) {} // eslint-disable-line
+
+                // create SocketIo connection
+                const socket = io({
+                    extraHeaders: {
+                        authorization: this.getters['user/jwt'],
+                    },
+                });
+
+                // listening events
+                socket.on('notes:changed', async () => {
+                    const { data } = await (
+                        this.getters.axios as AxiosInstance
+                    ).get('notes', {
+                        headers: {
+                            Authorization: this.getters['user/jwt'],
+                        },
+                    });
+
+                    state.notes = [...data.own, ...data.readonly];
+                });
+
+                socket.on('note:created', async (id: string) => {
+                    const { data } = await (
+                        this.getters.axios as AxiosInstance
+                    ).get(`notes/own/${id}`, {
+                        headers: {
+                            Authorization: this.getters['user/jwt'],
+                        },
+                    });
+
+                    state.notes.push(data);
+                });
+
+                socket.on('note:changed', async (id: string) => {
+                    const { data } = await (
+                        this.getters.axios as AxiosInstance
+                    ).get(`notes/own/${id}`, {
+                        headers: {
+                            Authorization: this.getters['user/jwt'],
+                        },
+                    });
+
+                    state.notes = state.notes.filter((n) => n.id !== id);
+                    state.notes.push(data);
+                });
+
+                socket.on('note:deleted', async (id: string) => {
+                    state.notes = state.notes.filter((n) => n.id !== id);
+                });
+
+                socket.on('note-readonly:subscribed', async (id: string) => {
+                    const { data } = await (
+                        this.getters.axios as AxiosInstance
+                    ).get(`notes/public/${id}`);
+
+                    state.notes.push(data);
+                });
+
+                socket.on('note-readonly:changed', async (id: string) => {
+                    const { data } = await (
+                        this.getters.axios as AxiosInstance
+                    ).get(`notes/public/${id}`);
+
+                    state.notes = state.notes.filter((n) => n.id !== id);
+                    state.notes.push(data);
+                });
+
+                socket.on('note-readonly:unsubscribed', async (id: string) => {
+                    state.notes = state.notes.filter((n) => n.id !== id);
+                });
+
+                state.socket = socket;
+            } catch (error) {
+                console.log(error);
+            } // eslint-disable-line
         },
     },
 
--- production.dockerfile
+++ production.dockerfile
@@ -9,43 +9,43 @@ WORKDIR /app
 
 RUN apt-get update && \
     apt-get install -y \
-        ca-certificates \
-        fonts-liberation \
-        libappindicator3-1 \
-        libasound2 \
-        libatk-bridge2.0-0 \
-        libatk1.0-0 \
-        libc6 \
-        libcairo2 \
-        libcups2 \
-        libdbus-1-3 \
-        libexpat1 \
-        libfontconfig1 \
-        libgbm1 \
-        libgcc1 \
-        libglib2.0-0 \
-        libgtk-3-0 \
-        libnspr4 \
-        libnss3 \
-        libpango-1.0-0 \
-        libpangocairo-1.0-0 \
-        libstdc++6 \
-        libx11-6 \
-        libx11-xcb1 \
-        libxcb1 \
-        libxcomposite1 \
-        libxcursor1 \
-        libxdamage1 \
-        libxext6 \
-        libxfixes3 \
-        libxi6 \
-        libxrandr2 \
-        libxrender1 \
-        libxss1 \
-        libxtst6 \
-        lsb-release \
-        wget \
-        xdg-utils && \
+    ca-certificates \
+    fonts-liberation \
+    libappindicator3-1 \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libc6 \
+    libcairo2 \
+    libcups2 \
+    libdbus-1-3 \
+    libexpat1 \
+    libfontconfig1 \
+    libgbm1 \
+    libgcc1 \
+    libglib2.0-0 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libpango-1.0-0 \
+    libpangocairo-1.0-0 \
+    libstdc++6 \
+    libx11-6 \
+    libx11-xcb1 \
+    libxcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxi6 \
+    libxrandr2 \
+    libxrender1 \
+    libxss1 \
+    libxtst6 \
+    lsb-release \
+    wget \
+    xdg-utils && \
     apt-get -f install && \
     rm -r -d -f /var/lib/apt/lists/*
 
@@ -54,6 +54,13 @@ COPY production/dist/ ./dist/
 COPY production/package.json .
 
 RUN npm install --save
+
+RUN groupadd -r node \
+    && useradd -r -g node -G audio,video node \
+    && chown -R node:node /app 
+
+USER node
+WORKDIR /app
 
 RUN pwd
 RUN ls -la
